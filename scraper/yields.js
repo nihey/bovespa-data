409b9443d9dee92948ae5c72898f99e5862fd444
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-09-01 21:00:23 -0300

    Try to find out dividend yields of each FII

---
+++ scraper/yields.js
@@ -0,0 +1,37 @@
+var yieldScraper = require('../lib/yields'),
+    fiis = require('../data/fii'),
+    fs = require('fs');
+
+var current = 0;
+var max = 4;
+var wait = function(func) {
+  var tryInit = function() {
+    if (current < max) {
+      ++current;
+      func();
+      return;
+    }
+    setTimeout(tryInit, 1000);
+  };
+  tryInit();
+};
+
+var save = function() {
+  console.log(fiis);
+};
+
+var length = 0;
+fiis.forEach(function(fii, index) {
+  if (fii.yields) {
+    return;
+  }
+  wait(function() {
+    console.log(fii.code)
+    yieldScraper(fii.code.substr(0, 4), function(yields, unparsed) {
+      --current;
+      fiis[index].yields = yields;
+      fiis[index].unparsed = unparsed;
+      fs.writeFileSync('foo.json', JSON.stringify(fiis));
+    });
+  });
+});
